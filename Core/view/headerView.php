709c6aa5234e0c29a78a1a7b46297f7053cce44f
3arbouch <mohamed.benarbia@epfl.ch>
2014-05-02 11:49:19 +0200

    First Page View

---
+++ Core/view/headerView.php
@@ -0,0 +1,7 @@
+	    <div id="header">
+	      <div id="logo">
+	        <a href="<?php echo index.php; ?>">
+	          <img src="../Links/MusicFinder.png" alt="Music Finder" width="240" height="100" border="0" />
+	        </a>
+	      </div>
+	    </div>
